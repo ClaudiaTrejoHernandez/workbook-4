27ab6c3449a8d5ff0261f0c1ecdfd3697eecd4c5
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-05 15:20:56 -0700

    Workbook4: Module2: Section2-1: Classes
    -initial commit
    -created hotel operations app to practice making classes
    -created privative variables, constructors, getters and setters in each class
    -also created methods as necessary

---
+++ HotelOpperations/src/main/java/com/pluralsight/Reservation.java
@@ -0,0 +1,52 @@
+package com.pluralsight;
+
+//Responsible for storing information related to a guest stay.
+public class Reservation {
+
+    private String roomType;
+    private double price;
+    private int numberOfNights;
+    boolean isWeekend = false;
+    private double reservationTotal;
+
+    public Reservation(String roomType, double price, int numberOfNights, boolean isWeekend, double reservationTotal) {
+        this.roomType = roomType;
+        this.price = price;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+        this.reservationTotal = reservationTotal;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setIsWeekend(boolean isWeekend) {
+        this.isWeekend = isWeekend;
+    }
+
+    public double getReservationTotal() {
+        return reservationTotal;
+    }
+
+}
---
+++ HotelOpperations/src/main/java/com/pluralsight/Room.java
@@ -0,0 +1,60 @@
+package com.pluralsight;
+
+//Responsible for knowing everything related to a hotel room.
+//A room is only available if it is clean and not currently occupied.
+public class Room {
+
+    private int numberOfBeds;
+    private double price;
+    boolean isOccupied = false;
+    boolean isDirty = false;
+    boolean isAvailable = true;
+
+    public Room(int numberOfBeds, double price, boolean isOccupied, boolean isDirty, boolean isAvailable) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.isOccupied = isOccupied;
+        this.isDirty = isDirty;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        isDirty = dirty;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+}
