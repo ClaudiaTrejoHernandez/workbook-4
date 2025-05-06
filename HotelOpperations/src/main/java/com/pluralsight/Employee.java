95d8b6eea819b719ff5cd28ac6f89924cf81fb39
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-06 10:06:30 -0700

    Changed syntax and removed unnecessary getters/setters
    -changed employeeId to a String
    -removed getters and setters not need for this exercise
    -created "derived getters" which don't need a backing variable

--- HotelOpperations/src/main/java/com/pluralsight/Employee.java
+++ HotelOpperations/src/main/java/com/pluralsight/Employee.java
@@ -3,57 +3,17 @@ package com.pluralsight;
 //Used to store and calculate payroll info about an employee.
 public class Employee {
 
-    private int employeeId;
+    private String employeeId;
     private String name;
     private String department;
     private double payRate;
     private double hoursWorked;
 
-    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+    public Employee(String employeeId, String name, String department, double payRate, double hoursWorked) {
         this.employeeId = employeeId;
         this.name = name;
         this.department = department;
         this.payRate = payRate;
-        this.hoursWorked = hoursWorked;
-    }
-
-    public int getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public double getPayRate() {
-        return payRate;
-    }
-
-    public void setPayRate(double payRate) {
-        this.payRate = payRate;
-    }
-
-    public double getHoursWorked() {
-        return hoursWorked;
-    }
-
-    public void setHoursWorked(double hoursWorked) {
         this.hoursWorked = hoursWorked;
     }
 
@@ -75,7 +35,7 @@ public class Employee {
 
     public double getTotalPay() {
         double regularPay = getRegularHours() * payRate;
-        double overTimePay = (1.5 * payRate) * getOvertimeHours();
+        double overTimePay = getOvertimeHours() * payRate * 1.5;
         return regularPay + overTimePay;
     }
 
